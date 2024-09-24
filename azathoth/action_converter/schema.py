5cc9fe529264b997b5229bbdcb80e414d21c801a
stevieflyer <steveflyer7@gmail.com>
2024-09-24 12:22:48 +0000

    fix

--- azathoth/action_converter/schema.py
+++ azathoth/action_converter/schema.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 
-from autom import AutomSchema, AutomField, autom_registry, WebUnsupportedFilepath
+from autom import AutomSchema, AutomField, autom_registry
 
 
 class FileActionConvertParams(AutomSchema):
-    autom_frontend_root_path: WebUnsupportedFilepath
-    api_src_fullpath: WebUnsupportedFilepath
-    action_dst_fullpath: WebUnsupportedFilepath
+    autom_frontend_root_path: Path
+    api_src_fullpath: Path
+    action_dst_fullpath: Path
 
 
 @autom_registry(is_internal=False)
 class AutomProjectActionConvertParams(AutomSchema):
-    autom_frontend_root_path: WebUnsupportedFilepath = AutomField(
+    autom_frontend_root_path: Path = AutomField(
         ...,
         description="The root path of the Autom Frontend Project",
     )
@@ -21,8 +21,8 @@ ProjectActionConvertPlannerInput = AutomProjectActionConvertParams
 
 
 class ProjectActionConvertPlan(AutomSchema):
-    autom_frontend_root_path: WebUnsupportedFilepath = AutomField(
+    autom_frontend_root_path: Path = AutomField(
         ...,
         description="The root path of the Autom Frontend Project",
     )
-    src_dst_filepaths_pair: list[tuple[WebUnsupportedFilepath, WebUnsupportedFilepath]]
+    src_dst_filepaths_pair: list[tuple[Path, Path]]
